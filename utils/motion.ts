8ba5b98e0c7f36311aeaf0be59f25325318c668e
Abdul Hai Rawan <abdulhairawan@gmail.com>
2024-06-29 17:29:27 +0430

    feedback-section completed

--- utils/motion.ts
+++ utils/motion.ts
@@ -135,7 +135,7 @@ export const planetVariants = (direction: string) => ({
   },
 });
 
-export const zoomIn = (delay: string, duration: string) => ({
+export const zoomIn = (delay: number, duration: number) => ({
   hidden: {
     scale: 0,
     opacity: 0,
