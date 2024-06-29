6de00804902b5244a08724af21d5bb14c75849e0
Abdul Hai Rawan <abdulhairawan@gmail.com>
2024-06-29 15:30:44 +0430

    world-section completed

--- components/sections/world.tsx
+++ components/sections/world.tsx
@@ -1,5 +1,52 @@
 "use client";
 
-const World = () => <section>World section</section>;
+import { TitleText, TypingText } from "../custom-texts";
+import { fadeIn, staggerContainer } from "@/utils/motion";
+
+import { motion } from "framer-motion";
+
+const World = () => (
+  <section className="paddings relative z-10">
+    <motion.div
+      variants={staggerContainer(0.3, 0.2)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="innerWidth mx-auto flex flex-col"
+    >
+      <TypingText title="| People on the World" className="text-center" />
+      <TitleText
+        title={
+          <>
+            Track friends around you and invite them to play together in the
+            same world
+          </>
+        }
+        className="text-center"
+      />
+
+      <motion.div
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        className="relative mt-[68px] flex w-full h-[550px]"
+      >
+        <img
+          src="/map.png"
+          alt="map"
+          className="w-full h-full object-contain"
+        />
+
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-01.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-02.png" alt="people" className="w-full h-full" />
+        </div>
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-03.png" alt="people" className="w-full h-full" />
+        </div>
+      </motion.div>
+    </motion.div>
+  </section>
+);
 
 export default World;
