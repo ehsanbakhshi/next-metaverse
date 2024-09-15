e5b59b2cb35848bd85faa51c4da4167611e34963
Abdul Hai Rawan <abdulhairawan@gmail.com>
2024-09-15 12:18:30 +0430

    text issues fixed

--- components/sections/whats-new.tsx
+++ components/sections/whats-new.tsx
@@ -20,8 +20,8 @@ const WhatsNew = () => (
         variants={fadeIn("right", "tween", 0.2, 1)}
         className="flex-[0.75] flex justify-center flex-col"
       >
-        <TypingText title="| What's New?" />
-        <TitleText title={<>What's new about Metaverses?</>} />
+        <TypingText title="| Whats New?" />
+        <TitleText title={<>What&apos;s new about Metaverses?</>} />
         <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
           {newFeatures.map((feature, index) => (
             <NewFeatures key={index} data={feature} />
